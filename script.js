516f9cf7ba0216259ac175df96e492c523408ef4
Nelson Kwesi Xedzro <nelsonxedzro660@gmail.com>
2024-09-22 20:00:16 +0000

    Update script.js

--- script.js
+++ script.js
@@ -35,9 +35,3 @@ menuIcon.onclick = () => {
 
 
 
-const menuIcon = document.getElementById('menu-icon');
-const navbar = document.querySelector('.navbar');
-
-menuIcon.addEventListener('click', () => {
-    navbar.classList.toggle('active'); // Toggle the 'active' class
-});
